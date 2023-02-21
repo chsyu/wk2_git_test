42430352e27a9e4c0a9473787fd6eee574fe7476
chsyu <chsyu@tea.ntue.edu.tw>
2023-02-21 10:59:21 +0800

    修改HEADER內容

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <header className="header">
         <div className="container d-flex flex-column align-items-center">
           <img src="images/chsyu.jpg" alt="" className="header__image" />
-          <h1 className="header__title">山‘<strong>s</strong> 網頁</h1>
+          <h1 className="header__title">WK2 GIT TEST</h1>
           <hr className="divider--light" />
           <p className="header__slogan">APP／WEB／WOT／UX</p>
         </div>
